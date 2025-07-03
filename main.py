4de4d905f16611d537b2488fe377b8d0faf8a8f6
upahsaka <upahsaka@gmail.com>
2025-07-03 22:14:06 +0400

    Update main.py

--- main.py
+++ main.py
@@ -80,6 +80,12 @@ async def main():
     logging.info("Тест-бот запущен.")
     await application.run_polling()
 
-if __name__ == '__main__':
-    import asyncio
-    asyncio.get_event_loop().run_until_complete(main())
+import asyncio
+
+if __name__ == "__main__":
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
