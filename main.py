56625b9267281acdeb583c250a4fdb3a26e11a54
upahsaka <upahsaka@gmail.com>
2025-07-03 22:17:42 +0400

    Update main.py

--- main.py
+++ main.py
@@ -82,10 +82,12 @@ async def main():
 
 import asyncio
 
+
 if __name__ == "__main__":
-    try:
-        loop = asyncio.get_event_loop()
-    except RuntimeError:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-    loop.run_until_complete(main())
+    import asyncio
+
+    import nest_asyncio
+    nest_asyncio.apply()
+
+    asyncio.run(main())
+
