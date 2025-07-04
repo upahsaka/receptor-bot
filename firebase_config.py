cec2204b9cb27bf6b7e678be949dd6f38227df1a
upahsaka <upahsaka@gmail.com>
2025-07-04 21:10:31 +0400

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, db
 import os
+import json
 
-cred = credentials.Certificate("firebase_key.json")  # Переименуй твой JSON-файл так
+cred_dict = json.loads(os.environ["FIREBASE_KEY_JSON"])
+cred = credentials.Certificate(cred_dict)
+
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://receptorfirebase-default-rtdb.firebaseio.com/'
 })
