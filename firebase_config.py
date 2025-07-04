9df73d61165df72ad92c7d2470e17611503a8d64
upahsaka <upahsaka@gmail.com>
2025-07-04 22:16:38 +0400

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,9 +3,25 @@ from firebase_admin import credentials, db
 import os
 import json
 
+# Получаем JSON из переменной окружения
 cred_dict = json.loads(os.environ["FIREBASE_KEY_JSON"])
-cred = credentials.Certificate(cred_dict)
 
+# Инициализируем Firebase
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://receptorfirebase-default-rtdb.firebaseio.com/'
 })
+
+# === Функции ===
+
+def load_history():
+    ref = db.reference("history")
+    history = ref.get()
+    if history is None:
+        history = {"smoothies": [], "recipes": [], "image_index": 0}
+        ref.set(history)
+    return history
+
+def save_history(history):
+    ref = db.reference("history")
+    ref.set(history)
